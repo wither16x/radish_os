59b64177d102cb6f72f808feeb662c61a5b61aee
Wither__ <hhex16@gmail.com>
2026-05-16 14:27:15 +0200

    More advanced exception handler

--- kernel/cpu/exception.cpp
+++ kernel/cpu/exception.cpp
@@ -1,10 +1,63 @@
 #include <cpu/assembly.hpp>
+#include <lib/typing.hpp>
 #include <panic.hpp>
 
 using namespace kernel;
 using namespace kernel::lib;
 
-extern "C" [[gnu::noreturn]] void exception_handler()
+namespace {
+
+// Registers and values pushed by the CPU before calling
+// `exception_handler()`
+struct [[gnu::packed]] cpu_frame {
+	u64 cr2;
+	u64 cr3;
+	u64 rax;
+	u64 rbx;
+	u64 rcx;
+	u64 rdx;
+	u64 rsi;
+	u64 rdi;
+	u64 rbp;
+	u64 r8;
+	u64 r9;
+	u64 r10;
+	u64 r11;
+	u64 r12;
+	u64 r13;
+	u64 r14;
+	u64 r15;
+	u64 err_type;
+	u64 err_code;
+	u64 rip;
+	u64 cs;
+	u64 flags;
+	u64 rsp;
+	u64 ss;
+};
+
+} /* anonymous namespace */
+
+extern "C" [[gnu::noreturn]] void exception_handler(cpu_frame *f)
 {
-        panic("CPU exception");
+        panic(
+                "CPU exception #%u with error code %u\r\n"
+                "RAX=0x%x RBX=0x%x RCX=0x%x RDX=0x%x\r\n"
+                "RDI=0x%x RSI=0x%x\r\n"
+                "RBP=0x%x RSP=0x%x\r\n"
+                "R8=0x%x R9=0x%x R10=0x%x R11=0x%x R12=0x%x R13=0x%x R14=0x%x R15=0x%x\r\n"
+                "CR2=0x%x CR3=0x%x\r\n"
+                "CS=0x%x SS=0x%x\r\n"
+                "RIP=0x%x\r\n"
+                "FLAGS=0x%x\r\n",
+                f->err_type, f->err_code,
+                f->rax, f->rbx, f->rcx, f->rdx,
+                f->rdi, f->rsi,
+                f->rbp, f->rsp,
+                f->r8, f->r9, f->r10, f->r11, f->r12, f->r13, f->r14, f->r15,
+                f->cr2, f->cr3,
+                f->cs, f->ss,
+                f->rip,
+                f->flags
+        );
 }
--- kernel/cpu/isr.asm
+++ kernel/cpu/isr.asm
@@ -1,12 +1,16 @@
 %macro isr_error_stub 1
 global __isr_stub%1
 __isr_stub%1:
+        push %1
+        ; the CPU pushes an error code
         jmp isr_common
 %endmacro
 
 %macro isr_no_error_stub 1
 global __isr_stub%1
 __isr_stub%1:
+        push %1
+        push 0 ; fake error code
         jmp isr_common
 %endmacro
 
@@ -17,6 +21,47 @@ extern exception_handler
 
 isr_no_error_stub       3               ; breakpoint
 
+; stack before jump:
+; - error code
+; - isr number
 isr_common:
+        push r15
+        push r14
+        push r13
+        push r12
+        push r11
+        push r10
+        push r9
+        push r8
+        push rbp
+        push rdi
+        push rsi
+        push rdx
+        push rcx
+        push rbx
+        push rax
+        mov rax, cr3
+        push rax
+        mov rax, cr2
+        push rax
+        mov rdi, rsp
         call exception_handler
+        pop rax
+        pop rax
+        pop rax
+        pop rbx
+        pop rcx
+        pop rdx
+        pop rsi
+        pop rdi
+        pop r15
+        pop r8
+        pop r9
+        pop r10
+        pop r11
+        pop r12
+        pop r13
+        pop r14
+        pop r15
+        add rsp, 16
         iretq
