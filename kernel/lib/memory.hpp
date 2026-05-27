c0d0161ae05f69f85a9a929a30127b3171032bb5
Wither__ <hhex16@gmail.com>
2026-05-27 20:05:19 +0200

    Added lib::move() and lib::copy()

--- kernel/lib/memory.hpp
+++ kernel/lib/memory.hpp
@@ -13,4 +13,42 @@ int memcmp(const void *s1, const void *s2, usize n);
 
 }
 
+// for copy() and move()
+//
+// remove_reference
+// -----------------------------------------------------------
+template<typename T>
+struct remove_reference {
+        using type = T;
+};
+
+template<typename T>
+struct remove_reference<T&> {
+        using type = T;
+};
+
+template<typename T>
+struct remove_reference<T&&> {
+        using type = T; 
+};
+
+template<typename T>
+using remove_reference_t = typename remove_reference<T>::type;
+// -----------------------------------------------------------
+
+template<typename T>
+T *copy(const T *first, const T *last, T *dest)
+{
+        while (first != last)
+                *dest++ = *first++;
+
+        return dest;
+}
+
+template<typename T>
+constexpr remove_reference_t<T>&& move(T&& value) noexcept
+{
+        return static_cast<remove_reference_t<T>&&>(value);
+}
+
 } /* namespace kernel::lib */
