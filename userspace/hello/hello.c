db94b18ea8d94480b10a132e1c298441972525bf
Wither__ <hhex16@gmail.com>
2026-07-07 13:41:30 +0200

    userspace/hello/hello.c uses stdio.h

--- userspace/hello/hello.c
+++ userspace/hello/hello.c
@@ -1,14 +1,19 @@
+#include <stdio.h>
 #include <unistd.h>
 
 int main()
 {
+        const char *name = "Wither__";
+        int age = 15;
+        printf("Hello! My name is %s and I am %d years old!\r\n", name, age);
+
         char buf;
-        write("D:/console", "Type some text: ", 16);
+        printf("Type some text: ");
 
         while (true) {
                 read("D:/input", &buf, sizeof(buf));
                 if (buf)
-                        write("D:/console", &buf, sizeof(buf));
+                        putchar(buf);
         }
 
         return 0;
