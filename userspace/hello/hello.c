5cf3449fd8a458a0a7e3d3330111801cdf82cd3c
Wither__ <hhex16@gmail.com>
2026-07-08 11:54:58 +0200

    In user test program: now using getchar() instead of read()

--- userspace/hello/hello.c
+++ userspace/hello/hello.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 int main()
 {
-        const char *name = "Wither__";
-        int age = 15;
-        printf("Hello! My name is %s and I am %d years old!\r\n", name, age);
-
-        char buf;
         printf("Type some text: ");
 
         while (true) {
-                read("D:/input", &buf, sizeof(buf));
-                if (buf)
-                        putchar(buf);
+                int ch = getchar();
+                putchar(ch);
         }
-
+        
         return 0;
 }
