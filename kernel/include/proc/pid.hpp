c365afb872bcb34b54e33c579caab2ac97686d1f
Wither__ <hhex16@gmail.com>
2026-07-24 16:40:12 +0200

    Documented kernel::proc::PID

--- kernel/include/proc/pid.hpp
+++ kernel/include/proc/pid.hpp
@@ -4,6 +4,8 @@
 
 namespace kernel::proc {
 
+/// Prefer using this type instead of `usize` so that the reader understands directly
+/// that the data is a PID and not a random unsigned integer.
 using PID = lib::usize;
 
 PID allocate_pid();
