1c47b5c1ce47603737f5c8a8db85fd62a8a08d1c
Wither__ <hhex16@gmail.com>
2026-08-15 11:06:27 +0200

    Still trying

--- scripts/build-cxx-libs.sh
+++ scripts/build-cxx-libs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# This script will install libc++, libc++abi and libunwind
+# This script will install libc++abi and libunwind
 # $1 = sysroot
+
+# clang > 19.x is required
 
 mkdir -p .build_cxx_libs
 
@@ -13,7 +15,7 @@ set -e
 
 cd .build_cxx_libs/
 cmake -G Ninja -S llvm-project/runtimes -B build \
-        -DLLVM_ENABLE_RUNTIMES="libunwind;libcxxabi" \
+        -DLLVM_ENABLE_RUNTIMES="libunwind;libcxxabi;libcxx" \
         -DCMAKE_C_COMPILER="clang" \
         -DCMAKE_CXX_COMPILER="clang++" \
         -DCMAKE_C_COMPILER_TARGET="x86_64-unknown-none" \
@@ -22,7 +24,9 @@ cmake -G Ninja -S llvm-project/runtimes -B build \
         -DCMAKE_INSTALL_PREFIX="$1" \
         -DLIBCXXABI_USE_LLVM_UNWINDER=ON \
         -DLIBCXX_CXX_ABI=libcxxabi \
+        -DLIBCXX_ENABLE_THREADS=OFF \
         -DLIBCXXABI_ENABLE_THREADS=OFF \
+        -DLIBCXX_INCLUDE_TESTS=OFF \
         -DLIBCXXABI_INCLUDE_TESTS=OFF \
         -DLIBUNWIND_INCLUDE_TESTS=OFF \
         -DCMAKE_SYSTEM_NAME=Generic \
@@ -30,8 +34,8 @@ cmake -G Ninja -S llvm-project/runtimes -B build \
         -DLIBUNWIND_ENABLE_THREADS=OFF \
         -DLIBUNWIND_IS_BAREMETAL=ON \
         -DLIBUNWIND_ENABLE_CROSS_UNWINDING=OFF \
-        -DCMAKE_C_FLAGS="-Wno-error=return-type" \
-        -DCMAKE_CXX_FLAGS="-Wno-error=return-type" \
-        -DCXX_SUPPORTS_WERROR_EQ_RETURN_TYPE_FLAG=0
-
-ninja -C build install-cxxabi install-unwind
+        -DCXX_SUPPORTS_WERROR_EQ_RETURN_TYPE_FLAG=0 \
+        -DCMAKE_C_FLAGS="-Wno-error=return-type -D_LIBCPP_DISABLE_VISIBILITY_ANNOTATIONS" \
+        -DCMAKE_CXX_FLAGS="-Wno-error=return-type -D_LIBCPP_DISABLE_VISIBILITY_ANNOTATIONS"
+        
+ninja -C build install-cxx install-cxxabi install-unwind
