6f2732a3d24f579261f84f399b1edb8a14aba094
Wither__ <hhex16@gmail.com>
2026-08-29 19:56:22 +0200

    Better init

--- userspace/init/init.c
+++ userspace/init/init.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+
+#define CASH_PATH               "I:/bin/cash"
 
 int main()
 {
-        char buf[50];
-        const char *shell_file = "I:/bin/cash";
+        /* arguments passed to the shell */
+        const char *argv[] = {
+                CASH_PATH
+        };
 
-        printf("Hello from init!\n");
+        printf("init: executing cash...\n");
 
-        FILE *f = fopen("I:/README.txt");
-        if (!f) {
-                printf("Failed to create file object\n");
-                goto exec_shell;
-        }
-        read(f->fd, buf, sizeof(buf));
-        fclose(f);
-
-        buf[strlen(buf) - 1] = '\0';
-        printf("I:/README.txt: %s\n", buf);
-
-exec_shell:
-        printf("Executing shell...\n");
         pid_t pid = fork();
-        if (pid == 0) {
-                char *argv[] = {shell_file};
-                exec(shell_file, 1, argv, NULL);
-        } else {
+        if (pid == 0)
+                exec(CASH_PATH, 1, argv, NULL);
+        else
                 wait();
-        }
 
         return 0;
 }
