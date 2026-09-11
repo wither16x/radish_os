8d864da78c6c0850a2d0c97e9bea8f337507418a
Wither__ <hhex16@gmail.com>
2026-09-11 19:24:24 +0200

    Fixed tinyexpr patch script

--- ports/tinyexpr/tinyexpr.sh
+++ ports/tinyexpr/tinyexpr.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cd ../origins/tinyexpr
-git apply ../../tinyexpr/0001-Ported-to-RadishOS.patch
+cd ports/origins/tinyexpr
+git apply ../../tinyexpr/patches/0001-Ported-to-RadishOS.patch
 cd -
