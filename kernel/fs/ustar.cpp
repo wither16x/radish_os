743c1657be5a75e5c3752265a627fb7a2eff2e52
Wither__ <hhex16@gmail.com>
2026-09-13 17:26:17 +0200

    Fix typo

--- kernel/fs/ustar.cpp
+++ kernel/fs/ustar.cpp
@@ -1,4 +1,3 @@
-#include "lib/logging.hpp"
 #include <fs/path.hpp>
 #include <fs/ustar.hpp>
 #include <fs/vfs.hpp>
@@ -178,19 +177,13 @@ namespace Kiwi::Fs::Ustar
         {
                 if (not this->owner or not this->owner->storage or not this->owner->storage->data)
                         return Vfs::Status::NullData;
-
                 if (this->cursor >= this->owner->storage->size)
                         return Vfs::Status::Eof;
 
-                Lib::usize remaining = this->owner->storage->size - this->cursor;
-                Lib::usize to_copy = n < remaining ? n : remaining;
+                Lib::usize rem = this->owner->storage->size - this->cursor;
+                Lib::usize to_copy = n < rem ? n : rem;
 
-                Lib::memcpy(
-                        buf,
-                        reinterpret_cast<Lib::u8 *>(this->owner->storage->data) + this->cursor,
-                        to_copy
-                );
-
+                Lib::memcpy(buf, reinterpret_cast<Lib::u8 *>(this->owner->storage->data) + this->cursor, to_copy);
                 this->cursor += to_copy;
 
                 return Vfs::Status::Success;
