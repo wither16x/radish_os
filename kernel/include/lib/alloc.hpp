79f9b9830f89fcee1be938ed86e2027a59bd658f
Wither__ <hhex16@gmail.com>
2026-09-23 15:33:50 +0200

    Removed Kiwi::Lib::malloc() and Kiwi::Lib::free().
    * Removed malloc() and free() since they are useless.
    * Removed noexcept from new/delete operators since it is useless because the kernel compiles with -fno-exceptions.

--- kernel/include/lib/alloc.hpp
+++ kernel/include/lib/alloc.hpp
@@ -2,39 +2,22 @@
 
 #include <lib/typing.hpp>
 
-namespace Kiwi::Lib
-{
-        /// Allocate `n` bytes on the heap and return a pointer to the
-        /// base address of the allocated area.
-        void *malloc(usize n);
-        /// Free a pointer which has been previously allocated on the
-        /// heap.
-        void free(void *p);
-} // namespace Kiwi::Lib
-
-// `new` and `delete` cannot be declared into a namespace so they are declared
-// here.
-// We also do not use kernel standard types here to not make the compiler scream
-// of pain.
-
 void *operator new(size_t size);
-void operator delete(void *ptr) noexcept;
+void operator delete(void *ptr);
 
 void *operator new[](size_t size);
-void operator delete[](void *ptr) noexcept;
+void operator delete[](void *ptr);
 
-// below, placement new/delete
-
-inline void *operator new(size_t, void *p) noexcept
+inline void *operator new(size_t, void *p)
 {
         return p;
 }
 
-inline void operator delete(void *, void *) noexcept {}
+inline void operator delete(void *, void *) {}
 
-inline void *operator new[](size_t, void *p) noexcept
+inline void *operator new[](size_t, void *p)
 {
         return p;
 }
 
-inline void operator delete[](void *, void *) noexcept {}
+inline void operator delete[](void *, void *) {}
--- kernel/lib/alloc.cpp
+++ kernel/lib/alloc.cpp
@@ -3,32 +3,19 @@
 #include <kernel.hpp>
 #include <panic.hpp>
 
-namespace Kiwi::Lib
-{
-        void *malloc(usize n)
-        {
-                return Mem::Heap::allocate(n);
-        }
-
-        void free(void *p)
-        {
-                Mem::Heap::free(p);
-        }
-} // namespace Kiwi::Lib
-
 void *operator new(size_t size)
 {
         if (not Kiwi::kcontext().heap_available)
                 Kiwi::panic("no heap available");
 
-        void *p = Kiwi::Lib::malloc(size);
+        void *p = Kiwi::Mem::Heap::allocate(size);
         return p;
 }
 
-void operator delete(void *ptr) noexcept
+void operator delete(void *ptr)
 {
         if (ptr)
-                Kiwi::Lib::free(ptr);
+                Kiwi::Mem::Heap::free(ptr);
 }
 
 void *operator new[](size_t size)
@@ -36,11 +23,11 @@ void *operator new[](size_t size)
         if (not Kiwi::kcontext().heap_available)
                 Kiwi::panic("no heap available");
 
-        return Kiwi::Lib::malloc(size);
+        return Kiwi::Mem::Heap::allocate(size);
 }
 
-void operator delete[](void *ptr) noexcept
+void operator delete[](void *ptr)
 {
         if (ptr)
-                Kiwi::Lib::free(ptr);
+                Kiwi::Mem::Heap::free(ptr);
 }
